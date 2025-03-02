e49d922ec24fcc924a3b7697bf2bab00dceb4981
RohanAbhilash <rohanabhilash2003@gmail.com>
2025-03-02 21:42:54 +0530

    updated_logs

---
+++ RESYNTH_AREA_RECOVER 1/config.mk
@@ -0,0 +1,16 @@
+# Platform and Design Configuration
+export PLATFORM         = sky130hd
+
+export DESIGN_NAME      = sha256_unrolled_pipelined
+
+export VERILOG_FILES    = $(sort $(wildcard ./designs/src/$(DESIGN_NAME)/sha256_unrolled_pipelined.v))
+
+export SDC_FILE         = ./designs/$(PLATFORM)/$(DESIGN_NAME)/constraint.sdc
+
+export CORE_UTILIZATION = 55
+export TNS_END_PERCENT = 100
+
+export REMOVE_ABC_BUFFERS = 1
+
+export PLACE_DENSITY=0.6
+export RESYNTH_AREA_RECOVER=1
